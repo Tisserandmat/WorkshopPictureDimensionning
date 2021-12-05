5110bf8ba96167aa679dba60eea3e53d657d0c8a
Nicolas Rousset <nrousset@gmail.com>
2021-12-05 22:41:05 +0100

    Adding mockito tests

--- src/test/java/org/aenori/SpringBatchJobWorkshop/controller/MediaControllerTest.java
+++ src/test/java/org/aenori/SpringBatchJobWorkshop/controller/MediaControllerTest.java
@@ -1,6 +1,9 @@
 package org.aenori.SpringBatchJobWorkshop.controller;
 
+import org.aenori.SpringBatchJobWorkshop.services.MediaService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,20 +12,29 @@ import org.springframework.ui.Model;
 
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @Transactional
 @ActiveProfiles("test")
 @SpringBootTest
 class MediaControllerTest {
-    @Autowired
+    @InjectMocks
     MediaController mediaController;
+
+    @Mock
+    MediaService mediaService;
 
     @Test
     void getAll() {
         Model model = new ExtendedModelMap();
+        when(mediaService.getMediaList()).thenReturn(new ArrayList<>());
         mediaController.getAll(model);
         assertTrue(model.containsAttribute("allElements"));
+        verify(mediaService).getMediaList();
     }
 
     @Test
